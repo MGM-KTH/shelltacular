c57e3754950fd913f7a9c48521c92e40c4c0560e
Gustaf Lindstedt <gustaflindstedt@gmail.com>
2014-04-26 17:55:54 +0200

    now reads line into char **args

--- shell.c
+++ shell.c
@@ -16,12 +16,15 @@
 #include <signal.h>
 #include <string.h>
 
-#define BUFSIZE 256
+#define BUFSIZE 70
+#define ARGSIZE 6
 #define PROMPT "$"
 
-void command_loop();
+void loop();
 void newline();
 void prompt();
+void printargs(char **args);
+char *getargs(char *buffer, char **args);
 
 /*
  * Register a signal handler
@@ -46,45 +49,110 @@ void register_sighandler( int signal_code, void (*handler) (int sig) )
 
 void sigint_handler(int sig)
 {
-    fputs("\n", stdout);
-    command_loop();
+	fputs("\n", stdout);
+	loop();
 }
 
 int main(int argc, char **argv)
 {
-    register_sighandler(SIGINT, sigint_handler);
-    command_loop();
-    return 0;
+	register_sighandler(SIGINT, sigint_handler);
+	loop();
+	return 0;
 }
 
-void command_loop()
+void loop()
 {
-    char command_buffer[BUFSIZE];
+	char line_buffer[BUFSIZE];
+	char *args[ARGSIZE];
+	char *cmd;
 
-    /* Start by outputing prompt */
-    prompt();
+	/* Start by outputing prompt */
+	prompt();
 
-    while(fgets(command_buffer, BUFSIZE, stdin)) { 
-        newline();
-        prompt();
-    }
-    newline(); /* Prettier exit with newline */
+	for(;fgets(line_buffer, BUFSIZE, stdin); cmd=NULL) { 
+
+		cmd = getargs(line_buffer, args);
+
+		if(NULL != cmd) {
+
+			printargs(args);
+
+		}
+		newline();
+		prompt();
+	}
+	newline(); /* Prettier exit with newline */
+}
+
+/*
+ * Debug method
+ */
+void printargs(char **args)
+{
+	int i;
+	for(i = 0; i < ARGSIZE && *args!=NULL; ++args, ++i) {
+		fprintf(stdout, "arg %d: %s\n", i, *args);
+	}
+}
+
+/*
+ * Extract the arguments from the buffer into args
+ *
+ * returns the first token or NULL if no tokens were parsed
+ */
+char *getargs(char *buffer, char **args)
+{
+	int i;
+	char *token;
+	char *string;
+	char *saveptr;
+
+	char **ptr = args;
+
+	for(i = 0, string = buffer;
+		i < ARGSIZE;
+		string = NULL, token = NULL, i++, ptr++) {
+
+		/*
+		 * String should be the string to be parsed the first time
+		 * the call is made. Every subsequent call string should
+		 * be NULL.
+		 */
+		token = strtok_r(string, " ", &saveptr);
+
+		/*
+		 * 0x0A is the LF character, which apparently is the first
+		 * token in an empty line, and the token returned if line
+		 * ends with a space character.
+		 */
+		if(NULL == token || !strcmp("\x0A",token)) {
+			/* No more tokens*/
+			break;
+		}else if(!strcmp("\x0A",token) && i == 0) {
+			/* Empty command */
+			return NULL;
+		}
+
+		*ptr = token;
+	}
+
+	return args[0];
 }
 
 void newline()
 {
-    int retval;
-    retval = fputs("\n", stdout);
-    if(EOF == retval) {
-        exit(0);
-    }
+	int retval;
+	retval = fputs("\n", stdout);
+	if(EOF == retval) {
+		exit(0);
+	}
 }
 
 void prompt()
 {
-    int retval;
-    retval = fputs(PROMPT, stdout);
-    if(EOF == retval) {
-        exit(0);
-    }
+	int retval;
+	retval = fputs(PROMPT, stdout);
+	if(EOF == retval) {
+		exit(0);
+	}
 }
